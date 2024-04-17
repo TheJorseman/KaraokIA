38abf4f26a53d4bb57bbe44d5f9ee57db14ac209
Miguel <miguel17546mals@gmail.com>
2024-04-17 03:36:17 -0600

    add documentation

--- Karaokia/download/youtube_download.py
+++ Karaokia/download/youtube_download.py
@@ -4,7 +4,17 @@ import librosa
 import soundfile as sf
 from yt_dlp import YoutubeDL
 
-def download_youtube(url, path="./downloads"):
+def download_youtube(url: str, path: str = "./downloads"):
+    """
+    Download a youtube video in the best mp4 format and return all the metadata of the video.
+
+    Args:
+        url (str): URL of the video
+        path (str, optional): Path to download the video. Defaults to "./downloads".
+
+    Returns:
+        dict: Metadata of the video
+    """
     if not os.path.exists(path):
         os.makedirs(path)
     outtmpl = os.path.join(path, '%(id)s.mp4')
@@ -25,6 +35,13 @@ def download_youtube(url, path="./downloads"):
         #error_code = ydl.download(url)
     return data
 
-def extract_audio_from_video(video_path, audio_path):
-   audio , sr = librosa.load(video_path)
-   sf.write(audio_path, audio, sr)
+def extract_audio_from_video(video_path: str, audio_path: str):
+    """
+    Extract the audio form a video.
+
+    Args:
+        video_path (str): Video path
+        audio_path (str): Path to save the extracted audio
+    """
+    audio , sr = librosa.load(video_path)
+    sf.write(audio_path, audio, sr)
